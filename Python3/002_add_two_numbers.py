0a5b5af6403a859c736248f1ce3f65bbe7624793
TrenywanG <brtxbrc0521@126.com>
2020-08-25 11:32:50 +0800

    添加笔记

--- Python3/002_add_two_numbers.py
+++ Python3/002_add_two_numbers.py
@@ -8,6 +8,54 @@
 @Contact :   brtxbrc0521@126.com
 """
 # here put the import lib
+class ListNode():
+    def __init__(self, val):
+        if isinstance(val,int):
+            self.val = val
+            self.next = None
+
+        elif isinstance(val,list):
+            self.val = val[0]
+            self.next = None
+            cur = self
+            for i in val[1:]:
+                cur.next = ListNode(i)
+                cur = cur.next
+
+    def gatherAttrs(self):
+        return ", ".join("{}: {}".format(k, getattr(self, k)) for k in self.__dict__.keys())
+
+    def __str__(self):
+        return self.__class__.__name__+" {"+"{}".format(self.gatherAttrs())+"}"
+
 
 class Solution:
-    def twosum(self,nums,):
+    def addTwoNumbers(self,l1,l2,):
+        def list_str(list):
+            result = []
+            while list:
+                result.append(str(list.val))
+                list = list.next
+            return result
+
+        def str_list(res):
+            ResList = ListNode(int(res[0]))
+            dummy = ResList
+            for i in range(1, len(res)):
+                ResList.next = ListNode(int(res[i]))
+                ResList = ResList.next
+            return dummy
+
+
+        if isinstance(l1,list):
+            l1 = ListNode(l1)
+            l2 = ListNode(l2)
+
+        res1 = ''.join(list_str(l1))[::-1]
+        res2 = ''.join(list_str(l2))[::-1]
+        res = str(int(res1) + int(res2))[::-1]
+        return str_list(res)
+
+if __name__ == "__main__":
+    solution1=Solution()
+    print(solution1.addTwoNumbers([2,4,3],[5,6,4]))
